84789bb26f5f1044413785f361de9ac661b52f5e
cur1iosity <cur1iosity@cur1ioisty.com>
2024-09-05 22:29:42 +0300

    Fixed tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='hexway-hive-api',
-   version='0.1.1',
+   version='0.1.2',
    description='Small wrapper for HexWay Hive API',
    long_description=open('README.md').read(),
    long_description_content_type='text/markdown',
