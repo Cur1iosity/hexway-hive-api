3eafce2b60a9e677025810cb6cdd482491dbf558
Cur1iosity <cur1iosity@protonmail.com>
2025-06-10 21:58:26 +0300

    Add asynchronous client implementation

--- hexway_hive_api/rest/http_client/__init__.py
+++ hexway_hive_api/rest/http_client/__init__.py
@@ -1,7 +1,9 @@
 from hexway_hive_api.rest.http_client.http_client import HTTPClient
+from hexway_hive_api.rest.http_client.async_http_client import AsyncHTTPClient
 from hexway_hive_api.rest.http_client import exceptions
 
 __all__ = [
     'HTTPClient',
+    'AsyncHTTPClient',
     'exceptions',
 ]
